9cf4d01ca98639b8be933986c3d04c2a5c217f62
ssandif <ssandifer@umbc.edu>
2020-04-25 22:05:50 -0400

    Add ParallelRisk library

---
+++ Assets/Scripts/TestParallelRisk.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using ParallelRisk;
+
+public class TestParallelRisk : MonoBehaviour
+{
+    [SerializeField]
+    private int maxDepth = 1;
+
+    void Start()
+    {
+        BoardState board = Risk.StandardBoard();
+        Move move = Minimax.Serial<BoardState, Move>(board, maxDepth);
+
+        if (move.IsAttack)
+            Debug.Log($"Attack from {(Risk.Id)move.From.Id} to {(Risk.Id)move.To.Id}");
+        else
+            Debug.Log("PassTurn");
+    }
+}
